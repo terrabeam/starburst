26a96acdc53922ba364d982640e4df68dac99f68
terrabeam <terrabeam-projects@gmail.com>
2025-07-23 21:01:59 +0200

    compacting phase 3

--- 1120_arch-xfce-minimal-core.sh
+++ 1120_arch-xfce-minimal-core.sh
@@ -21,30 +21,12 @@ tput sgr0
 echo
 
 #settings
-sudo pacman -S --noconfirm --needed yuku-dot-files-git
-sudo pacman -S --noconfirm --needed yuku-sddm-simplicity-git
-sudo pacman -S --noconfirm --needed yuku-flat-remix-dark-git
+sudo pacman -S --noconfirm --needed yuku-dot-files-git yuku-sddm-simplicity-git yuku-flat-remix-dark-git
 #sudo pacman -S --noconfirm --needed edu-shells-git
 #sudo pacman -S --noconfirm --needed edu-xfce-git
 
 #theming
-sudo pacman -S --noconfirm --needed arc-gtk-theme
-sudo pacman -S --noconfirm --needed surfn-icons-git
-sudo pacman -S --noconfirm --needed hardcode-fixer-git
+sudo pacman -S --noconfirm --needed arc-gtk-theme surfn-icons-git hardcode-fixer-git
 
 #tools
-sudo pacman -S --noconfirm --needed archlinux-logout-git
-sudo pacman -S --noconfirm --needed arandr
-sudo pacman -S --noconfirm --needed catfish
-sudo pacman -S --noconfirm --needed dmenu
-sudo pacman -S --noconfirm --needed galculator
-sudo pacman -S --noconfirm --needed networkmanager
-sudo pacman -S --noconfirm --needed network-manager-applet
-sudo pacman -S --noconfirm --needed networkmanager-openvpn
-sudo pacman -S --noconfirm --needed nitrogen
-sudo pacman -S --noconfirm --needed numlockx
-sudo pacman -S --noconfirm --needed pavucontrol
-sudo pacman -S --noconfirm --needed playerctl
-sudo pacman -S --noconfirm --needed xcolor
-sudo pacman -S --noconfirm --needed xorg-xkill
-sudo pacman -S --noconfirm --needed gparted
+sudo pacman -S --noconfirm --needed archlinux-logout-git arandr catfish dmenu galculator networkmanager network-manager-applet networkmanager-openvpn nitrogen numlockx pavucontrol playerctl xcolor xorg-xkill gparted

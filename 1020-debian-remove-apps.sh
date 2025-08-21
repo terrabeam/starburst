19d994b311ff11b6a53d41a098b8270de653d46f
terrabeam <terrabeam-projects@gmail.com>
2025-08-21 20:26:19 +0200

    1020-deb removed kernel part

--- 1020-debian-remove-apps.sh
+++ 1020-debian-remove-apps.sh
@@ -26,50 +26,9 @@ tput setaf 1;
   echo "###############################################################################"
   echo "###      - KEEP LINUX KERNEL GET RID OF THE OTHER KERNELS"
   echo "###      - GET RID OF BROADCOM AND REALTEK DRIVERS"
-  echo "###      - GET RID OF CONKYS"
   echo "###############################################################################"
   tput sgr0
   
-  #######broadcom and realtek
-  sudo apt-get purge -y broadcom-sta-dkms r8168-dkms rtl8821cu-dkms
-  sudo apt-get autoremove -y
-  
-read -n1 -r -p "Press any key to continue..." key
-
-  # Ensure at least one kernel (linux-image-amd64 + linux-headers-amd64) remains
-  if dpkg -l | grep -q "^ii  linux-image-amd64" && dpkg -l | grep -q "^ii  linux-headers-amd64"; then
-
-      # Define kernels/headers you want to remove (common Debian flavors)
-      KERNELS_TO_REMOVE=(
-          linux-image-rt-amd64 linux-headers-rt-amd64
-          linux-image-cloud-amd64 linux-headers-cloud-amd64
-          linux-image-unsigned-* linux-headers-unsigned-*
-          linux-image-xanmod linux-headers-xanmod
-      )
-
-      remove_package() {
-          local package=$1
-          # Check if package is installed
-          if dpkg -l | grep -q "^ii  $package"; then
-              echo "$package is installed. Removing..."
-              sudo apt-get purge -y "$package"
-          else
-              echo "$package is not installed."
-          fi
-      }
-
-      # Loop over list and remove
-      for kernel in "${KERNELS_TO_REMOVE[@]}"; do
-          remove_package "$kernel"
-      done
-
-      # Clean up unused dependencies
-      sudo apt-get autoremove -y
-
-  else
-      echo "Cannot proceed: At least one kernel must remain installed."
-  fi
-
 read -n1 -r -p "Press any key to continue..." key
 
   echo
